ea5236252697cfe9039d7a5aa75b563ae3bed21d
Ivan Kara <ivanjuan222@gmail.com>
2024-09-05 10:56:25 +0700

    Fix ffmpeg version

--- docker/main/install_deps.sh
+++ docker/main/install_deps.sh
@@ -39,7 +39,7 @@ apt-get -qq install --no-install-recommends --no-install-suggests -y \
 
 # btbn-ffmpeg -> amd64
 if [[ "${TARGETARCH}" == "amd64" ]]; then
-    apt-get -qq install -y ffmpeg=7:5.1.6-0+deb12u1
+    apt-get -qq install -y ffmpeg=5.1.6-0+deb12u1
     # mkdir -p /usr/lib/btbn-ffmpeg
     # wget -qO btbn-ffmpeg.tar.xz "https://github.com/NickM-27/FFmpeg-Builds/releases/download/autobuild-2022-07-31-12-37/ffmpeg-n5.1-2-g915ef932a3-linux64-gpl-5.1.tar.xz"
     # tar -xf btbn-ffmpeg.tar.xz -C /usr/lib/btbn-ffmpeg --strip-components 1
